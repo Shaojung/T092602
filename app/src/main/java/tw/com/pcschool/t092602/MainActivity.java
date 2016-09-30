ae20f932f511a355866338b3880421eda5c5fd4a
Shaojung <abc@abc.com>
2016-09-30 19:05:03 +0800

    Add JSON Practice.

--- app/src/main/java/tw/com/pcschool/t092602/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t092602/MainActivity.java
@@ -10,6 +10,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,10 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
 
-        final StringRequest request = new StringRequest("http://www.google.com.", new Response.Listener<String>() {
+        final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.d("NET", response);
+
+                try {
+                    JSONArray array = new JSONArray(response);
+                    for (int i=0;i<array.length();i++)
+                    {
+                        JSONObject obj = array.getJSONObject(i);
+                        Log.d("NET" , "district:" + obj.getString("district"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         }, new Response.ErrorListener() {
             @Override
